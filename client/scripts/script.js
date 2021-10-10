cee0ed1819f3e568bfd99bcae33b2bef702da005
LaimaNam <laima.namajunaite@gmail.com>
2021-10-10 15:44:08 +0300

    input validation logic

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -8,6 +8,8 @@ const pizzaPrice = document.querySelector('#price');
 const pizzaHeat = document.querySelector('#heat');
 const pizzaToppings = document.querySelector('#toppings');
 const pizzaPhotoSelect = document.querySelector('#pizzaPhoto');
+const selectedImgOutput = document.querySelector('#selectedImg');
+const error = document.querySelectorAll('.error');
 
 // -- menu
 const menuOutput = document.querySelector('.menu-output');
@@ -18,17 +20,14 @@ const menufilter = document.querySelector('#filterMenu');
 // -- FUNCTIONS
 
 // -- -- SessionStorage
-
 //get items from storage
 const getItemsFromSessionStorage = () => {
   return JSON.parse(sessionStorage.getItem('menuItems'));
 };
-
 //update items in session storage
 const updateItemsOnSessionStorage = (arr) => {
   sessionStorage.setItem('menuItems', JSON.stringify(arr));
 };
-
 // -- logic helpers
 const itemsFromStorage = getItemsFromSessionStorage();
 
@@ -41,7 +40,6 @@ const pizzaHeatIconRenderer = (number) => {
   }
   return heat;
 };
-
 //pizza photo rendere
 const pizzaPhotoRender = (photo) => {
   if (photo === 'pizza1') {
@@ -52,9 +50,24 @@ const pizzaPhotoRender = (photo) => {
     return './assets/pizzaPhotos/pizzaPhoto3.jpg';
   } else if (photo === 'pizza4') {
     return './assets/pizzaPhotos/pizzaPhoto4.jpg';
+  } else {
+    return '';
   }
 };
-
+//pizza photo rendere
+const formPhotoRender = () => {
+  if (pizzaPhotoSelect.value === 'pizza1') {
+    selectedImgOutput.src = './assets/pizzaPhotos/pizzaPhoto1.jpg';
+  } else if (pizzaPhotoSelect.value === 'pizza2') {
+    selectedImgOutput.src = './assets/pizzaPhotos/pizzaPhoto2.jpg';
+  } else if (pizzaPhotoSelect.value === 'pizza3') {
+    selectedImgOutput.src = './assets/pizzaPhotos/pizzaPhoto3.jpg';
+  } else if (pizzaPhotoSelect.value === 'pizza4') {
+    selectedImgOutput.src = './assets/pizzaPhotos/pizzaPhoto4.jpg';
+  } else {
+    selectedImgOutput.src = '';
+  }
+};
 //menu rederer
 const renderMenu = (pizzaArray) => {
   menuOutput.innerHTML = pizzaArray.reduce((total, pizza) => {
@@ -64,10 +77,12 @@ const renderMenu = (pizzaArray) => {
       <img src="${pizzaPhotoRender(
         pizza.photo
       )}" alt="" class="menu-item__photo" />
-      <div class="pizza-heat">${pizzaHeatIconRenderer(+pizza.heat)}</div>
+      <div class="pizza-heat">${
+        pizza.heat ? pizzaHeatIconRenderer(+pizza.heat) : ''
+      }</div>
       <h5>${pizza.name}</h5>
       <span>${pizza.price}</span>
-      <span>${pizza.toppings}</span>
+      <span>${pizza.toppings.join(',')}</span>
     </div>
       `;
 
@@ -83,7 +98,7 @@ const renderMenu = (pizzaArray) => {
   });
 };
 
-//save item to session storage
+//save pizza to session storage
 const saveItemToSessionStorage = (e) => {
   e.preventDefault();
 
@@ -95,19 +110,25 @@ const saveItemToSessionStorage = (e) => {
 
   const pizzaItem = {
     name: pizzaName.value,
-    price: pizzaPrice.value,
-    heat: pizzaHeat.value,
-    toppings: pizzaToppings.value,
+    price: Number(pizzaPrice.value).toFixed(2),
+    heat: Number(pizzaHeat.value),
+    toppings: pizzaToppings.value.split(' '),
     photo: pizzaPhotoSelect.value,
   };
 
-  // adding pizza to the session storage
-  pizzaArr.push(pizzaItem);
+  if (validateFormInputs(pizzaItem)) {
+    // adding pizza to the session storage
+    pizzaArr.push(pizzaItem);
 
-  updateItemsOnSessionStorage(pizzaArr);
+    updateItemsOnSessionStorage(pizzaArr);
 
-  // resetting the form
-  form.reset();
+    // resetting the form and error messages if any
+    selectedImgOutput.src = '';
+    error.forEach((err) => {
+      err.innerText = '';
+    });
+    form.reset();
+  }
 };
 
 //delete item from session storage
@@ -121,10 +142,10 @@ const deleteItemFromSessionStorage = (id) => {
         indexOfPizzaToRemove = index;
       }
     });
-
+    //deleting item
     itemsFromStorage.splice(indexOfPizzaToRemove, 1);
 
-    //update
+    //update storage and UI
     updateItemsOnSessionStorage(itemsFromStorage);
     renderMenu(itemsFromStorage);
   } else {
@@ -133,7 +154,54 @@ const deleteItemFromSessionStorage = (id) => {
 };
 
 //validate inputs
-const validateFormInputs = () => {};
+const validateFormInputs = (pizzaItem) => {
+  let valid = true;
+  const { name, price, heat, toppings } = pizzaItem;
+
+  //name - must be string, unique, max-length 30
+  if (
+    typeof name === !'string' ||
+    itemsFromStorage.some((item) => item.name === name) ||
+    name.length > 30
+  ) {
+    showError(
+      'Pizza name must be unique and no longer than 30 symbols.',
+      '.name-error-msg'
+    );
+    valid = false;
+  }
+  //price - must be positive number, decimal points 2
+  if (price < 0 || isNaN(price)) {
+    showError(
+      'Pizza price must be a positive number. Ex. 15.99',
+      '.price-error-msg'
+    );
+    valid = false;
+  }
+  //heat - must be number, integer, range 1-3
+  if (isNaN(heat) || !Number.isInteger(heat) || heat > 3 || heat < 0) {
+    showError(
+      'Pizza heat range must be from 1 to 3 integer number.',
+      '.heat-error-msg'
+    );
+    valid = false;
+  }
+  //toppings - must be array, min-length 2
+  if (toppings.length < 2) {
+    showError(
+      'Pizza toppings length must be more than 2 words.',
+      '.toppings-error-msg'
+    );
+
+    valid = false;
+  }
+  return valid;
+};
+
+//create input validation error message
+const showError = (error, className) => {
+  document.querySelector(className).innerText = error;
+};
 
 // -- -- S O R T I N G
 // sort items by name
@@ -145,7 +213,7 @@ const sortByName = () => {
   return sortedByNameArr;
 };
 
-//filter select event
+//select filter: form by select option
 const filterMenuItems = (e) => {
   if (e.target.value === 'byPriceLowesFirst') {
     const sortedByLowestPrice = itemsFromStorage.sort(
@@ -163,12 +231,16 @@ const filterMenuItems = (e) => {
   } else if (e.target.value === 'byHeatMildest') {
     const sortedByMildest = itemsFromStorage.sort((a, b) => +a.heat - +b.heat);
     renderMenu(sortedByMildest);
+  } else {
+    sortByName();
   }
 };
 
 // -- -- E V E N T S
 document.addEventListener('DOMContentLoaded', () => {
   menufilter.addEventListener('change', filterMenuItems);
+
+  pizzaPhotoSelect.addEventListener('change', formPhotoRender);
 
   renderMenu(sortByName());
 
